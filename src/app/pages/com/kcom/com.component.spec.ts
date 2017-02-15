a6da91e6ce2d86d0fc3817e658a41e63e96c00cc
TKasekamp <toniskasekamp@gmail.com>
2017-02-15 11:23:46 +0200

    KCom component test with injected ComService
    Karma test setup complete!

--- src/app/pages/com/kcom/com.component.spec.ts
+++ src/app/pages/com/kcom/com.component.spec.ts
@@ -1,40 +1,74 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import {
+  inject,
+  async,
+  TestBed,
+  ComponentFixture
+} from '@angular/core/testing';
 import { Component } from '@angular/core';
 import {
-  async,
-  TestBed
-} from '@angular/core/testing';
+  BaseRequestOptions,
+  ConnectionBackend,
+  Http
+} from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
-import { ComModule } from './com.module';
+// Load the implementations that should be tested
+import {AppState} from "../../../app.service";
+// import { HomeComponent } from './home.component';
+// import { Title } from './title';
 import {KComComponent} from "./kcom.component";
+import {ComService} from "../../../theme/services/com/com.service";
 
-export function main() {
-   describe('KCom component', () => {
-    // Setting module for testing
-    // Disable old forms
+describe(`KCom`, () => {
+  let comp: KComComponent;
+  let fixture: ComponentFixture<KComComponent>;
 
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        declarations: [TestComponent],
-        imports: [KComComponent]
-      });
-    });
+  // async beforeEach
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [KComComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        BaseRequestOptions,
+        MockBackend,
+        ComService,
+        {
+          provide: Http,
+          useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        AppState,
 
-    it('should work',
-      async(() => {
-        TestBed
-          .compileComponents()
-          .then(() => {
-            let fixture = TestBed.createComponent(TestComponent);
-            let aboutDOMEl = fixture.debugElement.children[0].nativeElement;
+      ]
+    })
+      .compileComponents(); // compile template and css
+  }));
 
-              expect(aboutDOMEl.querySelectorAll('h2')[0].textContent).toEqual('Com subsystem page');
-          });
-        }));
-    });
-}
+  // synchronous beforeEach
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KComComponent);
+    comp = fixture.componentInstance;
 
-@Component({
-  selector: 'test-cmp',
-  template: '<sd-com></sd-com>'
-})
-class TestComponent {}
+    fixture.detectChanges(); // trigger initial data binding
+  });
+
+  // it('should have default data', () => {
+  //   expect(comp.localState).toEqual({ value: '' });
+  // });
+
+  it('should have a kcom object', () => {
+    expect(!!comp.kcom).toBeTruthy();
+  });
+
+  // it('should log ngOnInit', () => {
+  //   spyOn(console, 'log');
+  //   expect(console.log).not.toHaveBeenCalled();
+  //
+  //   comp.ngOnInit();
+  //   expect(console.log).toHaveBeenCalled();
+  // });
+
+});
