1c2b992a0be33678fc387d685fa5c62e79ad4c78
TKasekamp <toniskasekamp@gmail.com>
2017-02-15 10:13:04 +0200

    A test that actually fucking works. AAAAAAAAAAA

---
+++ src/a.spec.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by Tonis on 14.02.2017.
+ */
+
+import { TestBed } from '@angular/core/testing';
+import { App } from './app.component';
+
+  describe('A component', () => {
+
+  it("works", function () {
+      expect(true).toBeTruthy();
+    }
+
+  );
+})
+
+
